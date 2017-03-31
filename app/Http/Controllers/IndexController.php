2df105dbe90d4ee914ba99669ef64793dee7a227
arshak2016 <hovhannisyan006@gmail.com>
2017-03-31 15:45:07 +0400

    adding layouts

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Service;
+use App\Portfolio;
+
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
-        return view('layouts.site');
+        $pages = Page::all();
+        $portfolios = Portfolio::get(array('name', 'filter', 'images'));
+        $services = Service::where('id', '<', 20)->get();
+
+
+        return view('site.index');
     }
 }
---
+++ resources/views/site/header.blade.php
@@ -0,0 +1,27 @@
+<div class="navbar-default navbar-fixed-top" id="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img class="logo-1" src="images/logo.png" alt="LOGO">
+                <img class="logo-2" src="images/logo-2.png" alt="LOGO">
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <nav class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav navbar-right" id="top-nav">
+                <li class="current"><a href="#body">Home</a></li>
+                <li><a href="#about">About us</a></li>
+                <li><a href="#service">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</div>
